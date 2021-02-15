3078a01a037b4498574357c243450a0a0420e812
Sergio Vasquez <30962111+sergiovasquez122@users.noreply.github.com>
2021-02-14 18:25:25 -0800

    refactored code

--- assignment1/msort.hs
+++ assignment1/msort.hs
@@ -5,8 +5,7 @@ msort [x] = [x] -- list with single element is sorted
 msort xs = merge (msort lhs) (msort rhs) -- We recursively sort the lhs and rhs then merge them to have a sorted array
     where merge xs [] = xs -- rhs fully used, return the lhs side
           merge [] ys = ys -- lhs full used, return the rhs side
-          merge (x:xs') (y:ys') = if x < y -- both sides have atleast one element but smaller one on new list and then call the function again with the new lists
+          merge (x:xs') (y:ys') = if x <= y -- both sides have atleast one element but smaller one on new list and then call the function again with the new lists
                                  then x : merge xs' (y:ys') -- x was smaller cons it to list and then call merge with smaller xs' and ys' being the same size as the original call                               
 							     else y : merge (x:xs') ys' -- y was smaller cons it to list and then call merge with 
-          lhs = take (length xs `div` 2) xs -- lhs will get the n/2 elements starting from the front and going to the middle
-          rhs = drop (length xs `div` 2) xs -- rhs will get the n/2 elements starting from the middle + 1 and going to the end
+          (lhs, rhs) = splitAt (length xs `div` 2) xs -- split the array to two subarrays where the lhs contains the 0 .. n/2 elements of the original array and the rhs contains the n/2+1 .. n elements of the original array
