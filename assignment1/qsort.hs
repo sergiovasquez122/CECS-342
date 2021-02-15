84bd7024091f9ed8895a3083b531e9985daf2eb8
Sergio Vasquez <30962111+sergiovasquez122@users.noreply.github.com>
2021-02-14 18:04:23 -0800

    update Qsort

--- assignment1/qsort.hs
+++ assignment1/qsort.hs
@@ -2,6 +2,6 @@ module QSort where
 qsort :: Ord a => [a] -> [a] -- quick sort a list in haskell
 qsort [] = [] -- quicksort on the empty list is the empty list
 qsort (x:xs) = -- the case where we have at least an element
-    left ++ [x] ++ right -- concatenate the followed followed by the pivot followed by the rhs since lhs <= x < rhs
+    left ++ (x : right) -- concatenate the followed followed by the pivot followed by the rhs since lhs <= x < rhs
     where left = qsort (filter (<= x) xs) -- construct the lhs by using the elements <= the pivot
           right = qsort (filter (> x) xs) -- construct the rhs by using the elements > the pivot
