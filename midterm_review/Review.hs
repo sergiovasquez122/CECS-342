290aa69794ba505432a6096fdb2de3357da5e30c
Sergio Vasquez <30962111+sergiovasquez122@users.noreply.github.com>
2021-02-19 20:30:51 -0800

    partial review notes

---
+++ midterm_review/Review.hs
@@ -0,0 +1,59 @@
+module Review where
+
+-- 1. In Haskell the list [1, 2, 3] is just syntatic sugar for 
+-- a. 1 : (2 : 3)
+-- b. 1 : (2 : (3 : []))
+-- c. (1 : 2) : 3
+-- d. (([] : 1) : 2) : 3
+-- b is the correct answer
+
+-- 2. Evaluate foldr (+) 0 [1, 2, 3]
+-- a. 0
+-- b. 2
+-- c. 4
+-- d. 6
+-- d is the correct answer
+
+-- Evaluate the following expresssion
+-- 3. foldr (-) 0 [1, 2, 3]
+--  1 - (2 - (3 - 0))
+--  1 - (-1)
+--  2
+
+-- Which of the follwoing two Haskell functions are tail recursive?
+-- foldr
+-- foldl
+-- -none-
+-- -both-
+-- foldl is the correct answer
+
+-- evaluate the following expression
+-- map (: "") "hi!"
+-- ["h", "i", "!"]
+
+-- What's the type of the concatenation operator ++
+--  (++) :: [a] -> [a] -> [a]
+
+-- How would you add an element x to a list xs in Haskell?
+-- x : xs
+
+-- What language feature is used in Haskell to create lists?
+-- the data construcgtors ":" and "[]"
+
+-- Which of the following could be the type of a sort function in Haskell?
+-- sort :: Ord a => [a] -> [a]
+
+-- Which kind o recursion is an important concept in functional programming
+-- a. head recursion
+-- b. neck recursion
+-- c. arm recursion
+-- d. leg recursion
+-- e. tail recursion
+-- e is the correct answer
+
+-- What does it mean that Haskell is a pure functional programming language?
+-- The evaluation of expressions has no side-effects
+
+-- Which technique can be used to transform a recursive funciton into an equivalent tail recursive function?
+-- accumulating parameters
+
