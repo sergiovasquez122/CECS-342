2cf3e905984840e75979f9f13be4d13e74762d8e
Sergio Vasquez <30962111+sergiovasquez122@users.noreply.github.com>
2021-02-22 13:43:11 -0800

    added to notes

--- midterm_review/Review.hs
+++ midterm_review/Review.hs
@@ -8,6 +8,8 @@ module Review where
 -- b is the correct answer
 
 -- 2. Evaluate foldr (+) 0 [1, 2, 3]
+-- 1 : (2 : (3 : 0))
+-- 1 + (2 + (3 + 0))
 -- a. 0
 -- b. 2
 -- c. 4
@@ -20,9 +22,9 @@ module Review where
 --  1 - (-1)
 --  2
 
--- Which of the follwoing two Haskell functions are tail recursive?
--- foldr
--- foldl
+-- Which of the following two Haskell functions are tail recursive?
+-- foldr (non-tail recursive function)
+-- foldl (tail recursive function), foldl could be made in terms of foldr but not other way
 -- -none-
 -- -both-
 -- foldl is the correct answer
@@ -30,6 +32,10 @@ module Review where
 -- evaluate the following expression
 -- map (: "") "hi!"
 -- ["h", "i", "!"]
+-- 'h' : ('i' : ('!' : ''))
+-- 'h' (: "") ('i' (: "") ('!' (: "") ''))
+--  'h' : ('i' : ['!'])
+-- ['h', 'i', '!']
 
 -- What's the type of the concatenation operator ++
 --  (++) :: [a] -> [a] -> [a]
@@ -38,10 +44,12 @@ module Review where
 -- x : xs
 
 -- What language feature is used in Haskell to create lists?
--- the data construcgtors ":" and "[]"
+-- the data constructor ":" and "[]"
 
 -- Which of the following could be the type of a sort function in Haskell?
 -- sort :: Ord a => [a] -> [a]
+-- sort :: a => [a] -> [a] wrong
+-- sort :: Ord a => [a] -> [b] wrong
 
 -- Which kind o recursion is an important concept in functional programming
 -- a. head recursion
@@ -54,24 +62,30 @@ module Review where
 -- What does it mean that Haskell is a pure functional programming language?
 -- The evaluation of expressions has no side-effects
 
--- Which technique can be used to transform a recursive funciton into an equivalent tail recursive function?
+-- Which technique can be used to transform a recursive function into an equivalent tail recursive function?
 -- accumulating parameters
+-- product [] base = base
+-- product (x:xs) current = product xs (current * x)
+-- non-tail
+-- product [] = 1
+-- product (x:xs) = x * product xs
+-- x * x' * x'' * x''' * x''''
 
 -- 4. In the Haskell function definition
 -- product [] = 1
 -- product (x:xs) = x * product xs
 -- we used an example of
--- a. list concatentation
--- b. pattern matching
--- c. incorrect syntax
--- d. tail recursion
+-- a. list concatentation (wrong) 
+-- b. pattern matching (right)
+-- c. incorrect syntax (wrong)
+-- d. tail recursion (wrong)
 ---  b is the correct answer
 
 -- 5. Which Haskell language feature is used where other languages would use NULL
--- a. lazy evaluation
--- b. monads
--- c. algebraic data types, in particular co-products
--- d. type classes
+-- a. lazy evaluation (wrong)
+-- b. monads (wrong)
+-- c. algebraic data types, in particular co-products NULL | NOT NULL
+-- d. type classes (wrong)
 -- c is the correct answer
 
 -- 6. what makes a recursive function definition tail recursive?
@@ -82,16 +96,20 @@ module Review where
 -- c is the correct answer
 
 -- 7. Consider the Haskell function definition
--- const x _= x
+-- const x _ = x
 -- evaluate 
---  const (1+) 10 100
+-- const (1+) 10 100
 -- 101
+
+-- example of wildcard
+-- length [] = 0
+-- length (_:xs) = 1 + length xs
 
 -- 8. Consider the Haskell function definition
 -- const x _ = x
 -- what is the type of const?
 -- a. const :: a -> b -> c
--- b. const :: a -> b -> a
+-- b. const :: a -> b -> a (perfect)
 -- c. const :: a -> b -> b
 -- d. const :: a -> a -> b
 -- e. const :: b -> b -> a
@@ -104,11 +122,11 @@ module Review where
 --  3 is the correct answer
 
 -- 10. What does the function foldr (++) [] do?
--- a. concatenate two list
+-- a. concatenate two list(wrong)
 -- b. flatten a list of lists
--- c. reverse a list
--- d. count the elements of a list
--- e. nothing
+-- c. reverse a list(wrong)
+-- d. count the elements of a list(wrong)
+-- e. nothing(wrong)
 -- b is the correct answer
 
 -- 11. What does the following evaluate to 
